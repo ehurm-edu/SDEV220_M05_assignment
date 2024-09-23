1e71e44b9db62e3368707ab5ccbda3693bc2c96b
Erin Hurm <ehurm@ivytech.edu>
2024-09-22 23:48:20 -0500

    should be ready for database

--- newapp/models.py
+++ newapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+'''
+This class was written by Travis for our group project.
+'''
+
+class Fosters(models.Model):
+    fosterID = models.IntegerField(unique=True, primary_key=True)
+    fosterName = models.CharField(max_length=50)
+    fosterEmail = models.EmailField(max_length=70)
+    fosterPhone = models.CharField(max_length=10)
+    fosterStreet = models.CharField(max_length=50)
+    fosterCity = models.CharField(max_length=50)
+    fosterState = models.CharField(max_length=2)
+    fotserZip = models.CharField(max_length=5)
--- newapp/views.py
+++ newapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from newapp.models import *
 
 # Create your views here.
+def index(request): #SDEV 220
+    foster_details = Fosters.objects.all()
+    foster_dictionary = {'foster_key': foster_details}
+    return render(request, 'index.html', context=foster_dictionary)
